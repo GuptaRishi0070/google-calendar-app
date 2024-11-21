d978c8d612d281dcac0a4c1d014170d7f966ae7b
Gupta_Rishi <108511606+MG-0070@users.noreply.github.com>
2024-11-22 01:42:19 +0530

    add create event functionality for easy event management

---
+++ server/models/Event.js
@@ -0,0 +1,12 @@
+
+const mongoose = require("mongoose");
+
+const EventSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  description: { type: String },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+});
+
+module.exports = mongoose.model("Event", EventSchema);
